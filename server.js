d43e640bd92c8fa475c67dc0f4612d3935560062
Jose <josea@student.local>
2026-05-28 15:14:21 -0600

    feat: massive aesthetic upgrade, hide nav buttons, create secret routes

--- server.js
+++ server.js
@@ -757,7 +757,15 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/votar', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.get('/admin', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'admin.html'));
+});
+
+app.get('/developer', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'admin.html'));
 });
 
